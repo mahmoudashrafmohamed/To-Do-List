4d349e6bbc23241291fb8d55da91900667adff6d
mahmoud_ashraf <ashraaf.mahmoud@gmail.com>
2017-07-14 10:00:18 +0200

    add task activity

---
+++ app/src/main/java/com/mahmoud_ashraf/to_do_list/AddTaskActivity.java
@@ -0,0 +1,13 @@
+package com.mahmoud_ashraf.to_do_list;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AddTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+    }
+}
