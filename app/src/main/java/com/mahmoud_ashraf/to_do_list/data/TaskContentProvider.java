3406bc17b70e0643350168ca613be91e5d1029c8
mahmoud_ashraf <ashraaf.mahmoud@gmail.com>
2017-07-14 16:23:38 +0200

    add content provider and register it in manifest

---
+++ app/src/main/java/com/mahmoud_ashraf/to_do_list/data/TaskContentProvider.java
@@ -0,0 +1,74 @@
+package com.mahmoud_ashraf.to_do_list.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by mahmoud_ashraf on 7/14/2017.
+ */
+
+/**
+ * 1- Verify that TaskContentProvider extends from ContentProvider and implements required methods
+ */
+public class TaskContentProvider extends ContentProvider {
+
+    // Member variable for a TaskDbHelper that's initialized in the onCreate() method
+    private TaskDbHelper mTaskDbHelper;
+
+    /* onCreate() is where you should initialize anything you’ll need to setup
+    your underlying data source.
+    In this case, you’re working with a SQLite database, so you’ll need to
+    initialize a DbHelper to gain access to it.
+     */
+    @Override
+    public boolean onCreate() {
+        /**
+         * 2- Complete onCreate() and initialize a TaskDbhelper on startup
+         */
+        Context context = getContext();
+        mTaskDbHelper = new TaskDbHelper(context);
+        return true;
+    }
+
+
+    @Override
+    public Uri insert(@NonNull Uri uri, ContentValues values) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    @Override
+    public Cursor query(@NonNull Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    @Override
+    public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    @Override
+    public int update(@NonNull Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    @Override
+    public String getType(@NonNull Uri uri) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
